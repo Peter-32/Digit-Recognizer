a3f6d3f5e5812c1ee935cd430a7e071d1a4a023d
Peter Myers <peter@impactradius.com>
2016-07-13 00:11:32 -0700

    new features

--- script.R
+++ script.R
@@ -1,6 +1,10 @@
 #Setup
 
 
+#USE THESE LINES TO SAVE/LOAD DATA FRAMES
+#save(train,file="DigitRecognition/train.Rda")
+#load("DigitRecognition/train.Rda")
+
 #Load table
 #pixels range from (0 to 783)
 train <- read.table("DigitRecognition/train.csv",header=TRUE,sep=",")
@@ -57,16 +61,80 @@ lastNonBlackRow <- function(df, observation){
 #Return the column of the first non-black point. (0 to 27)
 #firstNonBlackColumn(train,2000)
 firstNonBlackColumn <- function(df, observation){
-  for (dfCol in 2:785 ) {
-    if (df[observation,dfCol] != 0){
-      
+  for (i in 0:27){
+    for (dfCol in seq(2+i,785,28) ) {
+      if (df[observation,dfCol] != 0){
+        return(i)
+      }
     }
   }
 }
 
 #Return the column of the last non-black point. (0 to 27)
 #lastNonBlackColumn(train,2000)
+lastNonBlackColumn <- function(df, observation){
+  for (i in 27:0){
+    for (dfCol in seq(29-i,785,28) ) {
+      if (df[observation,dfCol] != 0){
+        return(i)
+      }
+    }
+  }
+}
+
+# Set up the first four features
+observations <- dim(train)[1]
+#left
+train$left <- -1
+for (i in 1:observations) {
+  train$left[i] <- firstNonBlackColumn(train,i)  
+}
+#right
+train$right <- -1
+for (i in 1:observations) {
+  train$right[i] <- lastNonBlackColumn(train,i)  
+}
+#top
+train$top <- -1
+for (i in 1:observations) {
+  train$top[i] <- firstNonBlackRow(train,i)
+}
+#bottom
+train$bottom <- -1
+for (i in 1:observations) {
+  train$bottom[i] <- lastNonBlackRow(train,i)  
+}
+
+# NEXT FEATURE:
+# This loop will go through all points of the new box
+# the new box contains anything between left, right, top, bottom
+# These values can be found in the dataframe's 786 to 789 columns
+# To keep it from getting too messy they are passed as parameters.
+pctNonBlackBoxes <- function(df, observation, left, right, top, bottom){
+  nonBlackBoxes <- 0
+  totalBoxes <- (1 + bottom - top) * (1 + right - left)
+  for (i in left:right){
+    for (dfCol in seq(28*top + 2+i,28*bottom+1,28) ) {
+      if (df[observation,dfCol] != 0){
+        nonBlackBoxes <- nonBlackBoxes + 1
+      }
+    }
+  }
+  return(nonBlackBoxes/totalBoxes)
+}
+#Adding the feature using the function
+train$pctNonBlackBoxes <- -1
+for (i in 1:observations) {
+  train$pctNonBlackBoxes[i] <- pctNonBlackBoxes(train,i, train$left[i], train$right[i], train$top[i], train$bottom[i])  
+}
 
 
-
+#13 More features based on one pixel being nonBlack or black
+#The pixels are inside the new box defined by top, bottom, left, and right
+#These are the pixel locations
+#-   -   -
+#  -   -
+#-   -   -
+#  -   -
+#-   -   -
 
