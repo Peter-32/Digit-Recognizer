c0e950ef1dea798ff0df69762220dba15734a44c
Peter Myers <peter@impactradius.com>
2016-07-13 18:04:28 -0700

    submitted to Kaggle

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
 # Digit-Recognizer
+
+The Digit-Recognizer program will read an image and classify it as one of ten digits (0-9).  This program has a success rate on Kaggle's test dataset of 83.614%.
+
+## Details
+- Kaggle's test dataset has 28000 observations that need to be classified into one of ten possible categories (0-9).
+- The program takes an image of 784 pixels (28x28).
+- Each pixel ranges from 0 to 255 brightness.
+- The Quadratic Discriminant Analysis model fit uses 25 features.
+
+## Models submitted
+- Linear Discriminant Analysis without cross-validation gave a 79.9% success rate on the test set.
+- Quadratic Discriminant Analysis without cross-validation gave a 83.614% success rate on the test set.
+
+## Files
+- "script.R" holds the R code used to build the model and test the model.
+- The files "train.csv" and "test.csv" were used to train and test the model respectively.  
+- The files "test.Rda" and "train.Rda" are files that can be loaded into R with the features already added.
+- "SubmissionResultsKaggle.PNG" and "SubmissionResultsKaggle 2nd Attempt.PNG" have the Kaggle results.
+
+## Area of improvement for the model
+- Use 5-fold cross-validation on the training set.  This would very likely give a better success rate than 83.614%.
+
+## Feature Names:
+- pctNonBlackBoxes
+- pointOfInterest1IsNonBlack
+- pointOfInterest2IsNonBlack
+- pointOfInterest3IsNonBlack
+- pointOfInterest4IsNonBlack
+- pointOfInterest5IsNonBlack
+- pointOfInterest6IsNonBlack
+- pointOfInterest7IsNonBlack
+- pointOfInterest8IsNonBlack
+- pointOfInterest9IsNonBlack
+- pointOfInterest10IsNonBlack
+- pointOfInterest11IsNonBlack
+- pointOfInterest12IsNonBlack
+- pointOfInterest13IsNonBlack
+- width
+- height
+- countNewlyNonBlackOn3HorizontalLinesPath
+- countNewlyNonBlackOn3VerticalLinesPath
+- countNewlyNonBlackOnCrossXPath
+- pctNonBlackBoxesTopHalf
+- pctNonBlackBoxesBottomHalf
+- pctNonBlackBoxesLeftHalf
+- pctNonBlackBoxesRightHalf
+- pctNonBlackBoxesTopHalfDividedByBottomHalf
+- pctNonBlackBoxesLeftHalfDividedByRightHalf
--- script.R
+++ script.R
@@ -1,10 +1,14 @@
 #Setup
 # first run: install.packages("MASS")
-library("MASS")
+#            install.packages("ISLR")
+library(MASS)
+library(ISLR)
 
 #USE THESE LINES TO SAVE/LOAD DATA FRAMES
 #save(train,file="DigitRecognition/train.Rda")
 #load("DigitRecognition/train.Rda")
+#save(test,file="DigitRecognition/test.Rda")
+#load("DigitRecognition/test.Rda")
 
 #Load table
 #pixels range from (0 to 783)
@@ -377,8 +381,109 @@ for (i in 1:observations) {
 #pctNonBlackBoxesTopHalfDividedByBottomHalf
 #pctNonBlackBoxesLeftHalfDividedByRightHalf
 
+
+###########ALL FUNCTIONS IN ONE PLACE (BELOW)
+#### THESE FUNCTIONS ARE FOUND ABOVE AS WELL
+firstNonBlackRow <- function(df, observation){
+  for (dfCol in 2:785 ) {
+    if (df[observation,dfCol] != 0){
+      pixelIdx <- dfCol-2
+      return(  floor(pixelIdx / 28)  ) 
+    }
+  }
+}
+lastNonBlackRow <- function(df, observation){
+  for (dfCol in 785:2 ) {
+    if (df[observation,dfCol] != 0){
+      pixelIdx <- dfCol-2
+      return(  floor(pixelIdx / 28)  )
+    }
+  }
+}
+firstNonBlackColumn <- function(df, observation){
+  for (i in 0:27){
+    for (dfCol in seq(2+i,785,28) ) {
+      if (df[observation,dfCol] != 0){
+        return(i)
+      }
+    }
+  }
+}
+lastNonBlackColumn <- function(df, observation){
+  for (i in 27:0){
+    for (dfCol in seq(29-i,785,28) ) {
+      if (df[observation,dfCol] != 0){
+        return(i)
+      }
+    }
+  }
+}
+pctNonBlackBoxes <- function(df, observation, left, right, top, bottom){
+  nonBlackBoxes <- 0
+  totalBoxes <- (1 + bottom - top) * (1 + right - left)
+  for (i in left:right){
+    for (dfCol in seq(28*top + 2+i,28*bottom+1,28) ) {
+      if (df[observation,dfCol] != 0){
+        nonBlackBoxes <- nonBlackBoxes + 1
+      }
+    }
+  }
+  return(nonBlackBoxes/totalBoxes)
+}
+findMidpoint <- function(df, observation, start, end){
+  return(floor(0.5 * (start + end)))
+}
+pointIsNonBlackColor <- function(df, observation, row, col){
+  pixel <- col + row * 28
+  dfCol <- pixel + 2  #the column this pixel is stored.
+  if (df[observation,dfCol] != 0) {
+    return(1)
+  }
+  else {
+    return(0)
+  }
+}
+generateListOfDFColsOnPath3HorizontalLines <- function(midRow1, midRow2, midRow3){
+  pixels <- c( seq(midRow1 * 28, 27 + midRow1 * 28),
+               seq(midRow2 * 28, 27 + midRow2 * 28),
+               seq(midRow3 * 28, 27 + midRow3 * 28))
+  dfCols <- pixels + 2 #the column this pixel is stored.
+  return(dfCols)
+}
+generateListOfDFColsOnPath3VerticalLines <- function(midCol1, midCol2, midCol3){
+  pixels <- c( seq(midCol1,783,28),
+               seq(midCol2,783,28),
+               seq(midCol3,783,28))
+  dfCols <- pixels + 2 #the column this pixel is stored.
+  return(dfCols)
+}
+generateListOfDFColsOnPathCrossX <- function(bottom, left, top){
+  pixels <- c( seq(bottom * 28 + left, 28, -27),
+               seq(top * 28 + left, 783, 29))
+  dfCols <- pixels + 2 #the column this pixel is stored.
+  return(dfCols)
+}
+countNewlyNonBlacksOnPath <- function(df, observation, dfCols){
+  count <- 0
+  lastPixelBlack <- 1 #set as true initially
+  for (pixelValue in df[observation,dfCols]) {
+    if (pixelValue != 0 && lastPixelBlack == 1) {
+      count = count + 1
+    } 
+    if (pixelValue == 0) {
+      lastPixelBlack = 1
+    } else {
+      lastPixelBlack = 0
+    }
+  }
+  return(count)
+}
+###########ALL FUNCTIONS IN ONE PLACE (ABOVE)
+
 ####LDA#############
-#Try QDA NEXT
+#Can try QDA next after getting test error on the website
+
+#### INITIAL FIT
 lda.fit=lda(label ~ pctNonBlackBoxes + pointOfInterest1IsNonBlack + pointOfInterest2IsNonBlack +
               pointOfInterest3IsNonBlack + pointOfInterest4IsNonBlack + pointOfInterest5IsNonBlack +
               pointOfInterest6IsNonBlack + pointOfInterest7IsNonBlack + pointOfInterest8IsNonBlack +
@@ -388,4 +493,187 @@ lda.fit=lda(label ~ pctNonBlackBoxes + pointOfInterest1IsNonBlack + pointOfInter
               countNewlyNonBlackOnCrossXPath + pctNonBlackBoxesTopHalf + pctNonBlackBoxesBottomHalf +
               pctNonBlackBoxesLeftHalf + pctNonBlackBoxesRightHalf +
               pctNonBlackBoxesTopHalfDividedByBottomHalf +
-              pctNonBlackBoxesLeftHalfDividedByRightHalf,data=train,CV=TRUE)
+              pctNonBlackBoxesLeftHalfDividedByRightHalf,data=train,CV=FALSE)
+
+qda.fit=qda(label ~ pctNonBlackBoxes + pointOfInterest1IsNonBlack + pointOfInterest2IsNonBlack +
+              pointOfInterest3IsNonBlack + pointOfInterest4IsNonBlack + pointOfInterest5IsNonBlack +
+              pointOfInterest6IsNonBlack + pointOfInterest7IsNonBlack + pointOfInterest8IsNonBlack +
+              pointOfInterest9IsNonBlack + pointOfInterest10IsNonBlack + pointOfInterest11IsNonBlack +
+              pointOfInterest12IsNonBlack + pointOfInterest13IsNonBlack + width + height +
+              countNewlyNonBlackOn3HorizontalLinesPath + countNewlyNonBlackOn3VerticalLinesPath +
+              countNewlyNonBlackOnCrossXPath + pctNonBlackBoxesTopHalf + pctNonBlackBoxesBottomHalf +
+              pctNonBlackBoxesLeftHalf + pctNonBlackBoxesRightHalf +
+              pctNonBlackBoxesTopHalfDividedByBottomHalf +
+              pctNonBlackBoxesLeftHalfDividedByRightHalf,data=train,CV=FALSE)
+
+#GET TEST DATA READY BY LOADING FEATURES
+temp <- read.table("DigitRecognition/test.csv",header=TRUE,sep=",")
+test = data.frame(0,temp) ## The feature building requires a column in the first location
+###########GET ALL FEATURES FOR TEST SET (BELOW)
+# Set up the first four features
+observations <- dim(test)[1]
+#left
+test$left <- -1
+for (i in 1:observations) {
+  test$left[i] <- firstNonBlackColumn(test,i)  
+}
+#right
+test$right <- -1
+for (i in 1:observations) {
+  test$right[i] <- lastNonBlackColumn(test,i)  
+}
+#top
+test$top <- -1
+for (i in 1:observations) {
+  test$top[i] <- firstNonBlackRow(test,i)
+}
+#bottom
+test$bottom <- -1
+for (i in 1:observations) {
+  test$bottom[i] <- lastNonBlackRow(test,i)  
+}
+#Adding the feature using the function
+test$pctNonBlackBoxes <- -1
+for (i in 1:observations) {
+  test$pctNonBlackBoxes[i] <- pctNonBlackBoxes(test,i, test$left[i], test$right[i], test$top[i], test$bottom[i])  
+}
+#ADDING THE MID ROWS:
+test$midRow2 <- -1
+for (i in 1:observations) {
+  test$midRow2[i] <- findMidpoint(test,i,test$top[i],test$bottom[i])
+}
+test$midRow1 <- -1
+for (i in 1:observations) {
+  test$midRow1[i] <- findMidpoint(test,i,test$top[i],test$midRow2[i])
+}
+test$midRow3 <- -1
+for (i in 1:observations) {
+  test$midRow3[i] <- findMidpoint(test,i,test$midRow2[i],test$bottom[i])
+}
+#ADDING THE MID COLUMNS:
+test$midCol2 <- -1
+for (i in 1:observations) {
+  test$midCol2[i] <- findMidpoint(test,i,test$left[i],test$right[i])
+}
+test$midCol1 <- -1
+for (i in 1:observations) {
+  test$midCol1[i] <- findMidpoint(test,i,test$left[i],test$midCol2[i])
+}
+test$midCol3 <- -1
+for (i in 1:observations) {
+  test$midCol3[i] <- findMidpoint(test,i,test$midCol2[i],test$right[i])
+}
+#13 Points of interest
+test$pointOfInterest1IsNonBlack <- -1
+for (i in 1:observations) {
+  test$pointOfInterest1IsNonBlack[i] <- pointIsNonBlackColor(test, i, test$top[i], test$left[i])
+}
+test$pointOfInterest2IsNonBlack <- -1
+for (i in 1:observations) {
+  test$pointOfInterest2IsNonBlack[i] <- pointIsNonBlackColor(test, i, test$top[i], test$midCol2[i])
+}
+test$pointOfInterest3IsNonBlack <- -1
+for (i in 1:observations) {
+  test$pointOfInterest3IsNonBlack[i] <- pointIsNonBlackColor(test, i, test$top[i], test$right[i])
+}
+test$pointOfInterest4IsNonBlack <- -1
+for (i in 1:observations) {
+  test$pointOfInterest4IsNonBlack[i] <- pointIsNonBlackColor(test, i, test$midRow1[i], test$midCol1[i])
+}
+test$pointOfInterest5IsNonBlack <- -1
+for (i in 1:observations) {
+  test$pointOfInterest5IsNonBlack[i] <- pointIsNonBlackColor(test, i, test$midRow1[i], test$midCol3[i])
+}
+test$pointOfInterest6IsNonBlack <- -1
+for (i in 1:observations) {
+  test$pointOfInterest6IsNonBlack[i] <- pointIsNonBlackColor(test, i, test$midRow2[i], test$left[i])
+}
+test$pointOfInterest7IsNonBlack <- -1
+for (i in 1:observations) {
+  test$pointOfInterest7IsNonBlack[i] <- pointIsNonBlackColor(test, i, test$midRow2[i], test$midCol2[i])
+}
+test$pointOfInterest8IsNonBlack <- -1
+for (i in 1:observations) {
+  test$pointOfInterest8IsNonBlack[i] <- pointIsNonBlackColor(test, i, test$midRow2[i], test$right[i])
+}
+test$pointOfInterest9IsNonBlack <- -1
+for (i in 1:observations) {
+  test$pointOfInterest9IsNonBlack[i] <- pointIsNonBlackColor(test, i, test$midRow3[i], test$midCol1[i])
+}
+test$pointOfInterest10IsNonBlack <- -1
+for (i in 1:observations) {
+  test$pointOfInterest10IsNonBlack[i] <- pointIsNonBlackColor(test, i, test$midRow3[i], test$midCol3[i])
+}
+test$pointOfInterest11IsNonBlack <- -1
+for (i in 1:observations) {
+  test$pointOfInterest11IsNonBlack[i] <- pointIsNonBlackColor(test, i, test$bottom[i], test$left[i])
+}
+test$pointOfInterest12IsNonBlack <- -1
+for (i in 1:observations) {
+  test$pointOfInterest12IsNonBlack[i] <- pointIsNonBlackColor(test, i, test$bottom[i], test$midCol2[i])
+}
+test$pointOfInterest13IsNonBlack <- -1
+for (i in 1:observations) {
+  test$pointOfInterest13IsNonBlack[i] <- pointIsNonBlackColor(test, i, test$bottom[i], test$right[i])
+}
+test$width <- -1
+for (i in 1:observations) {
+  test$width[i] <- 1 + test$right[i] - test$left[i]
+}
+test$height <- -1
+for (i in 1:observations) {
+  test$height[i] <- 1 + test$bottom[i] - test$top[i]
+}
+test$countNewlyNonBlackOn3HorizontalLinesPath <- -1
+for (i in 1:observations) {
+  dfCols <- generateListOfDFColsOnPath3HorizontalLines(test$midRow1[i], test$midRow2[i], test$midRow3[i])
+  test$countNewlyNonBlackOn3HorizontalLinesPath[i] <- countNewlyNonBlacksOnPath(test, i, dfCols)
+}
+test$countNewlyNonBlackOn3VerticalLinesPath <- -1
+for (i in 1:observations) {
+  dfCols <- generateListOfDFColsOnPath3VerticalLines(test$midCol1[i], test$midCol2[i], test$midCol3[i])
+  test$countNewlyNonBlackOn3VerticalLinesPath[i] <- countNewlyNonBlacksOnPath(test, i, dfCols)
+}
+test$countNewlyNonBlackOnCrossXPath <- -1
+for (i in 1:observations) {
+  dfCols <- generateListOfDFColsOnPathCrossX(test$bottom[i], test$left[i], test$top[i])
+  test$countNewlyNonBlackOnCrossXPath[i] <- countNewlyNonBlacksOnPath(test, i, dfCols)
+}
+test$pctNonBlackBoxesTopHalf <- -1
+for (i in 1:observations) {
+  test$pctNonBlackBoxesTopHalf[i] <- pctNonBlackBoxes(test,i, test$left[i], test$right[i], test$top[i], test$midRow2[i])  
+}
+test$pctNonBlackBoxesBottomHalf <- -1
+for (i in 1:observations) {
+  test$pctNonBlackBoxesBottomHalf[i] <- pctNonBlackBoxes(test,i, test$left[i], test$right[i], test$midRow2[i], test$bottom[i])  
+}
+test$pctNonBlackBoxesLeftHalf <- -1
+for (i in 1:observations) {
+  test$pctNonBlackBoxesLeftHalf[i] <- pctNonBlackBoxes(test,i, test$left[i], test$midCol2[i], test$top[i], test$bottom[i])  
+}
+test$pctNonBlackBoxesRightHalf <- -1
+for (i in 1:observations) {
+  test$pctNonBlackBoxesRightHalf[i] <- pctNonBlackBoxes(test,i, test$midCol2[i], test$right[i], test$top[i], test$bottom[i])  
+}
+test$pctNonBlackBoxesTopHalfDividedByBottomHalf <- -1
+for (i in 1:observations) {
+  test$pctNonBlackBoxesTopHalfDividedByBottomHalf[i] <- test$pctNonBlackBoxesTopHalf[i] / test$pctNonBlackBoxesBottomHalf[i]
+}
+test$pctNonBlackBoxesLeftHalfDividedByRightHalf <- -1
+for (i in 1:observations) {
+  test$pctNonBlackBoxesLeftHalfDividedByRightHalf[i] <- test$pctNonBlackBoxesLeftHalf[i] / test$pctNonBlackBoxesRightHalf[i]
+}
+
+###########GET ALL FEATURES FOR TEST SET (ABOVE)
+
+lda.pred=predict(lda.fit, test)
+lda.class=lda.pred$class
+output <- data.frame(1:28000,lda.class)
+names(output) <- c("ImageId","Label")
+write.table(output, file="DigitRecognition/submission.csv",sep=",", row.names=TRUE, col.names=FALSE)
+
+qda.pred=predict(qda.fit, test)
+qda.class=qda.pred$class
+output2 <- data.frame(1:28000,qda.class)
+names(output2) <- c("ImageId","Label")
+write.table(output2, file="DigitRecognition/submission2.csv",sep=",", row.names=TRUE, col.names=FALSE)
